63668037f26786f97d1edc73ec7a6317e325669b
Natalia Mytsak <mytsaknat@gmail.com>
2024-02-09 12:08:35 +0200

    Methods draw and move created

--- main.py
+++ main.py
@@ -10,21 +10,32 @@ bg_color = (234, 218, 184)
 
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Arkanoid")
-
-
+clock = pygame.time.Clock()
+fps = 60
 
 class Game:
     def __init__(self):
         self.continueGame = True
         self.paddle = Paddle(width, height)
+        self.paddle.draw(window)
 
     def run(self):
+
+
+
         while self.continueGame:
-            window.fill(bg_color)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.continueGame = False
-            pygame.display.flip()
+
+            window.fill(bg_color)
+            self.paddle.move(width)
+            self.paddle.draw(window)
+
+            pygame.display.update()
+            clock.tick(fps)
+
 
         pygame.quit()
 
--- movable/paddle.py
+++ movable/paddle.py
@@ -3,11 +3,29 @@ from pygame.locals import Rect
 
 
 class Paddle:
-    def __init__(self, width, height):# screen width and height
+    def __init__(self, screen_width, screen_height):# screen width and height
         self.height = 20
-        self.width = int(width/6)# has to be changed based on number of colors
-        self.x = int((width/2) - (self.width/2))
-        self.y = height - (self.height*2)
-        self.speed = 10
+        self.width = int(screen_width/6)# has to be changed based on number of colors
+        self.x = int((screen_width/2) - (self.width/2))
+        self.y = screen_height - (self.height*2)
+        self.speed = 5
         self.rectangle = Rect(self.x, self.y, self.width, self.height)
         self.direction = 0
+        self.fill_color = (142, 135, 123)
+        self.outline_color = (100, 100, 100)
+
+
+    def move(self, screen_width):
+        self.direction = 0
+        key = pygame.key.get_pressed()
+        if key[pygame.K_LEFT] and self.rectangle.left>0:
+            self.rectangle.x -=self.speed
+            self.direction = -1
+        if key[pygame.K_RIGHT] and self.rectangle.right<screen_width:
+            self.rectangle.x +=self.speed
+            self.direction  =1
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.fill_color, self.rectangle)
+        pygame.draw.rect(screen, self.outline_color, self.rectangle, 3)
+
