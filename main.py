88606dba95714bc695c820821ca68d51b232781b
Natalia Mytsak <mytsaknat@gmail.com>
2024-02-09 10:03:58 +0200

    Paddle class created

--- main.py
+++ main.py
@@ -10,6 +10,16 @@ bg_color = (234, 218, 184)
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Arkanoid")
 
+class Paddle:
+    def __init__(self):
+        self.height = 20
+        self.width = int(width/6)# has to be changed based on number of colors
+        self.x = int((width/2) - (self.width/2))
+        self.y = height - (self.height*2)
+        self.speed = 10
+        self.rectangle = Rect(self.x, self.y, self.width, self.height)
+        self.direction = 0
+
 class Game:
     def __init__(self):
         self.continueGame = True
