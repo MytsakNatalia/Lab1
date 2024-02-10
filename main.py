18d68c12e6132de79a623cfb69c3600d41c06885
Anastasiia Levchenko <nastiya.levchenko@gmail.com>
2024-02-10 14:58:15 +0100

    Draw initial Wall

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 from movable.paddle import  Paddle
+from unmovable.wallBricks import Wall
 
 pygame.init()
 
@@ -8,16 +9,24 @@ width = 600
 height = 600
 bg_color = (234, 218, 184)
 
+cols = 6
+rows = 6
+
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Arkanoid")
 clock = pygame.time.Clock()
 fps = 60
 
+
 class Game:
     def __init__(self):
         self.continueGame = True
+        self.wall = Wall(width, height, bg_color, cols, rows )       
+        self.wall.create_wall()
+        self.wall.draw_wall(window)
         self.paddle = Paddle(width, height)
         self.paddle.draw(window)
+        
 
     def run(self):
 
@@ -30,6 +39,7 @@ class Game:
                     self.continueGame = False
 
             window.fill(bg_color)
+            self.wall.draw_wall(window)
             self.paddle.move(width)
             self.paddle.draw(window)
 
